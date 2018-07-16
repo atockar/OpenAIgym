cf747e5bf686f317e68a1a707661a4aa4535705a
Anthony Tockar <atockar@Anthonys-MacBook-Pro.local>
2018-07-17 09:45:34 +1000

    Initial commit

---
+++ JSAnimation/display_frames_as_gif.py
@@ -0,0 +1,18 @@
+from matplotlib import animation
+import matplotlib.pyplot as plt
+from IPython.display import display
+from JSAnimation.JSAnimation.IPython_display import display_animation
+
+def display_frames_as_gif(frames):
+    """
+    Displays a list of frames as a gif, with controls
+    """
+    plt.figure(figsize=(frames[0].shape[1] / 36.0, frames[0].shape[0] / 36.0), dpi = 72)
+    patch = plt.imshow(frames[0])
+    plt.axis('off')
+
+    def animate(i):
+        patch.set_data(frames[i])
+
+    anim = animation.FuncAnimation(plt.gcf(), animate, frames = len(frames), interval=50)
+    display(display_animation(anim, default_mode='loop'))
